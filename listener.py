76d483db5e4c48187185d026126091b3cfe8ce91
JennyZhao99 <yibozh@seas.upenn.edu>
2025-04-21 22:03:46 -0700

    modify listener.py

--- listener.py
+++ listener.py
@@ -49,15 +49,53 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         print( f"Scanning block {start_block} on {chain}" )
     else:
         print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
+    
+    log_file_exists = Path(eventfile).is_file()
+    log_data = []
 
     if end_block - start_block < 30:
         event_filter = contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters=arg_filter)
         events = event_filter.get_all_entries()
         #print( f"Got {len(events)} entries for block {block_num}" )
         # TODO YOUR CODE HERE
+        for evt in events:
+            block = w3.eth.get_block(evt.blockNumber)
+            timestamp = datetime.fromtimestamp(block.timestamp).strftime('%m/%d/%Y %H:%M:%S')
+            
+            data = {
+                'chain': chain,
+                'token': evt.args['token'],
+                'recipient': evt.args['recipient'],
+                'amount': evt.args['amount'],
+                'transactionHash': evt.transactionHash.hex(),
+                'address': evt.address,
+                'date': timestamp
+            }
+            log_data.append(data)
     else:
         for block_num in range(start_block,end_block+1):
             event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
             events = event_filter.get_all_entries()
             #print( f"Got {len(events)} entries for block {block_num}" )
             # TODO YOUR CODE HERE
+            for evt in events:
+                block = w3.eth.get_block(block_num)
+                timestamp = datetime.fromtimestamp(block.timestamp).strftime('%m/%d/%Y %H:%M:%S')
+                
+                data = {
+                    'chain': chain,
+                    'token': evt.args['token'],
+                    'recipient': evt.args['recipient'],
+                    'amount': evt.args['amount'],
+                    'transactionHash': evt.transactionHash.hex(),
+                    'address': evt.address,
+                    'date': timestamp
+                }
+                log_data.append(data)
+    
+        if log_data:
+            df = pd.DataFrame(log_data)
+            if log_file_exists:
+                df.to_csv(eventfile, mode='a', header=False, index=False)
+            else:
+                df.to_csv(eventfile, mode='w', header=True, index=False)
